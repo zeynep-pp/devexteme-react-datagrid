5b974eba15d5a3da328a9c7181fc94735031d03f
Zeynep Köse <zeynep.j17@gmail.com>
2024-07-25 15:15:10 +0300

    edit class component

--- src/pages/tasks/tasks.js
+++ src/pages/tasks/tasks.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import DataGrid, { Column, Editing } from 'devextreme-react/data-grid';
 import Button from 'devextreme-react/button';
 import PopupComponent from './PopupComponent'; // Ayrı bir popup bileşeni
@@ -8,62 +8,68 @@ const data = [
   // Diğer veriler
 ];
 
-const Tasks = () => {
-  const [gridData, setGridData] = useState(data);
-  const [popupData, setPopupData] = useState(null);
-  const [options1, setOptions1] = useState(['Option1', 'Option2', 'Option3']);
-  const [popupVisible, setPopupVisible] = useState(false);
+class Tasks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridData: data,
+      popupData: null,
+      options1: ['Option1', 'Option2', 'Option3'],
+      popupVisible: false,
+    };
+  }
 
-  const handleCellClick = (e) => {
+  handleCellClick = (e) => {
     if (e.column.dataField === 'combinedValue' || e.column.dataField === 'newCombinedValue') {
-      setPopupData({
-        id: e.row.data.id,
-        field: e.column.dataField,
-        value: e.row.data[e.column.dataField]
+      this.setState({
+        popupData: {
+          id: e.row.data.id,
+          field: e.column.dataField,
+          value: e.row.data[e.column.dataField],
+        },
+        popupVisible: true,
       });
-      setPopupVisible(true);
     }
   };
 
-  const handleSave = (id, field, newValue) => {
-    const updatedData = gridData.map(item => 
+  handleSave = (id, field, newValue) => {
+    const updatedData = this.state.gridData.map((item) =>
       item.id === id ? { ...item, [field]: newValue } : item
     );
-    setGridData(updatedData);
-    setPopupData(null);
-    setPopupVisible(false);
+    this.setState({
+      gridData: updatedData,
+      popupData: null,
+      popupVisible: false,
+    });
   };
 
-  return (
-    <div>
-      <Button text="Refresh" onClick={() => setGridData(data)} />
-      <DataGrid
-        dataSource={gridData}
-        keyExpr="id"
-        showBorders={true}
-        onCellClick={handleCellClick}
-      >
-        <Editing
-          mode="cell"
-          allowUpdating={true}
-          allowAdding={true}
-          allowDeleting={true}
-        />
-        <Column dataField="combinedValue" caption="Combined Value" />
-        <Column dataField="newCombinedValue" caption="New Combined Value" />
-      </DataGrid>
+  render() {
+    return (
+      <div>
+        <Button text="Refresh" onClick={() => this.setState({ gridData: data })} />
+        <DataGrid
+          dataSource={this.state.gridData}
+          keyExpr="id"
+          showBorders={true}
+          onCellClick={this.handleCellClick}
+        >
+          <Editing mode="cell" allowUpdating={true} allowAdding={true} allowDeleting={true} />
+          <Column dataField="combinedValue" caption="Combined Value" />
+          <Column dataField="newCombinedValue" caption="New Combined Value" />
+        </DataGrid>
 
-      {popupVisible && (
-        <PopupComponent 
-          data={popupData}
-          options1={options1}
-          visible={popupVisible}
-          onSave={handleSave}
-          onCancel={() => setPopupVisible(false)}
-        />
-      )}
-    </div>
-  );
-};
+        {this.state.popupVisible && (
+          <PopupComponent
+            data={this.state.popupData}
+            options1={this.state.options1}
+            visible={this.state.popupVisible}
+            onSave={this.handleSave}
+            onCancel={() => this.setState({ popupVisible: false })}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default Tasks;
